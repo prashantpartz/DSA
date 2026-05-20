09919a9439e0c7bd3dcb8cb6f02d46f56c8139de
Prashant Singh <prashantpartz@gmail.com>
2026-05-20 21:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (85.12%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     bool isGood(vector<int>& nums) {
     int n = nums.size();
-    
-    vector<int>freq(n+1,0);
-    for(int i = 0;i<n;i++){
-        if(nums[i]>n-1){
-            return false;
-        }
-        freq[nums[i]]++;
-
-    }
-    for(int i = 1;i<n-2;i++){ 
-    if(freq[i]!=1){
+    sort(nums.begin(),nums.end());
+    if(nums[n-1]!=n-1 ||nums[n-2]!=n-1){
         return false;
     }
+    
+    
+    for(int i = 0;i<n-2;i++){
+        if(nums[i]!=i+1){
+            return false;
+        }
     }
-    return freq[n-1]==2;
+
+    return true;
 
     
      
