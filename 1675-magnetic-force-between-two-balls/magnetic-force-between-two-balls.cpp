a4af7f63e6c05d1498135501f2603bf9a7167b40
Prashant Singh <prashantpartz@gmail.com>
2026-06-10 22:59:14 +0530

    Time: 43 ms (89.14%) | Memory: 61.8 MB (8.56%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,38 +1,41 @@
 class Solution {
+
 public:
-    bool ispossible(vector<int>& position, int m, int mid) {
+    bool ispossible (vector<int>& position, int m,int mid,int end){
+        int n = end;
         int cows = 1;
         int lastpos = position[0];
-
-        for (int i = 1; i < position.size(); i++) {
-            if (position[i] - lastpos >= mid) {
+        for(int i = 1;i<position.size();i++){
+            if((position[i]-lastpos)>=mid){
                 cows++;
                 lastpos = position[i];
             }
-            if (cows == m) {
+            if(cows==m){
                 return true;
+
             }
+            
         }
         return false;
     }
-
     int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(), position.end());
-
+        sort(position.begin(),position.end());
         int st = 1;
-        int end = position[position.size()-1] - position[0];
+        int cow = 0;
         int ans = 0;
-
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            if (ispossible(position, m, mid)) {
+        int mid = 0;
+        int end = position[position.size()-1];
+        while(st <= end){
+            mid = st + (end-st)/2;
+            if(ispossible(position,m,mid,end)){
                 ans = mid;
-                st = mid + 1;
-            } else {
-                end = mid - 1;
+                st = mid+1;
+                
+            }else{
+                end = mid-1;
             }
         }
         return ans;
+        
     }
 };
