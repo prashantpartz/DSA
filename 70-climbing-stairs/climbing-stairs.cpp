6a27898846943b3fb733af9d09ec6181e96b423b
Prashant Singh <prashantpartz@gmail.com>
2026-06-12 22:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (34.17%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if(n<=2){return n;}
+        vector<int> fib(n+1);
+        fib[1]= 1;
+        fib[2]= 2;
+        for(int i = 3;i<=n;i++){
+            fib[i]=fib[i-1]+fib[i-2];
+        }
+        return fib[n];
+
+    }
+};
