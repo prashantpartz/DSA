b835f803947ad02701c8f9a2674b71fcc230258a
Prashant Singh <prashantpartz@gmail.com>
2026-07-21 21:01:48 +0530

    Time: 3 ms (65.69%) | Memory: 7.9 MB (38.77%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,16 +4,14 @@ public:
         if(n==0||n==1){
             return n;
         }
-        int sum = 0 ;
         int prev = 0;
         int nxt = 0;
         int cur = 1;
         for(int i = 1 ; i<n ; i++){
-            sum =  prev + cur;
             nxt = prev + cur;
             prev = cur;
             cur = nxt;
         }
-        return sum ;
+        return nxt ;
     }
 };
