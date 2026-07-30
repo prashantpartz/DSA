c62091e4e732914a05a40040e85a5fd92eefa17e
Prashant Singh <prashantpartz@gmail.com>
2026-07-30 23:56:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (6.36%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+        int n = s.size();
+        vector<int> answer(n, INT_MAX);
+    
+        int prev = -1e9; 
+        for (int i = 0; i < n; i++) {
+            if (s[i] == c) prev = i;
+            answer[i] = i - prev;
+        }
+        
+        
+        prev = 1e9;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == c) prev = i;
+            answer[i] = min(answer[i], prev - i);
+        }
+        
+        return answer;
+    }
+};
+        
+    
